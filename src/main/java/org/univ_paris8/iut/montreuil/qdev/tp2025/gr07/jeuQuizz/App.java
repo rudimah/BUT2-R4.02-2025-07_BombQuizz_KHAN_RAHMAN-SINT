7bc0ec4ed48e984e6c1fd27e724c1079dbb95235
hrahman <hamidur.rahman@outlook.fr>
2025-04-08 17:00:29 +0200

    init

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr07/jeuQuizz/App.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr07/jeuQuizz/App.java
@@ -1,9 +1,5 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2025.gr07.jeuQuizz;
 
-import org.univ_paris8.iut.montreuil.qdev.tp2025.gr07.jeuQuizz.entities.dtos.JoueurDTO;
-
-import java.util.ArrayList;
-
 /**
  * Hello world!
  *
@@ -12,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        ArrayList<String> centreInteret = new ArrayList<>(); centreInteret.add("Jeux video");
-        JoueurDTO joueurDTO = new JoueurDTO("Hamiodur", "hraman", 22122004, centreInteret, 1);
+        System.out.println( "Hello World!" );
     }
 }
