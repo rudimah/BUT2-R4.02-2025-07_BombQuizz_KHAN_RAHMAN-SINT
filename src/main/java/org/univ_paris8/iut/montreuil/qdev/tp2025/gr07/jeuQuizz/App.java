5747ef601d0d70e9204ce46aacc90429fd4b30ca
hrahman <hamidur.rahman@outlook.fr>
2025-04-08 16:26:32 +0200

    init

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr07/jeuQuizz/App.java
@@ -0,0 +1,13 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2025.gr07.jeuQuizz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr07/jeuQuizz/AppTest.java
@@ -0,0 +1,21 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2025.gr07.jeuQuizz;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
